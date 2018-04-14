67696233f566eb1d278f4e99609441cfb2bcc993
gdev <guilhermefos.developer@gmail.com>
2018-04-14 03:15:44 -0300

    Add Firebase configuration

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    messagingSenderId: "116849377525",
+    storageBucket: "fir-9a92b.appspot.com",
+    databaseURL: "https://fir-9a92b.firebaseio.com",
+    authDomain: "fir-9a92b.firebaseapp.com",
+    projectId: "fir-9a92b",
+    apiKey: "AIzaSyBPqhSLTQdsi_TJ8qImMekktNlKE-vZbIY",
+}
